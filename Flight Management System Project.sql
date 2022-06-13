3c53f08e3c029ffb04595f71eb25e8976a10b785
SAURABH KIRANRAO DABHADE <98541032+saurabhdabhade@users.noreply.github.com>
2022-06-13 10:31:36 +0530

    Add files via upload

---
+++ Flight Management System Project.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Flight_Management_System.tickets (
+    id int NOT NULL AUTO_INCREMENT,
+    amount int,
+    passenger_name varchar(255),
+    nationality varchar(255),
+	gender varchar(255),
+    phone long,
+    PRIMARY KEY (id)
+);
+
+
+    
